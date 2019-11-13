eea17fc324a4f2ce5437bd3d490762e6b52532d6
kiga-coder <54422469+kiga-coder@users.noreply.github.com>
2019-11-12 19:58:46 -0500

    Actualizacion documentos
    Impresion de numeros en stylo LCD

---
+++ SofkaLCD/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+
+namespace SofkaLCD
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int i, j, n;
+            n = 10; 
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine("       --   --    --");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine("     |   |    |  |");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine("     |   |    |  |");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine("       --   -- -- --");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine("     ||       |  |  |");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine("     ||       |  |  |");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine("       --   --    --");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine("         ");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine("  ---  ---  ---  ---  ---");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine(" |        ||   ||   ||   |");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine(" |        ||   ||   ||   |");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine(" |        ||   ||   ||   |");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine("  ---       ---  --- ");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine(" |        ||   ||   ||   |");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine(" |        ||   ||   ||   |");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine(" |        ||   ||   ||   |");
+                }
+            }
+
+            for (i = 1; i < n + (n - 18); i++)
+            {
+                for (j = n + (n - 18); j > i; j--)
+                {
+                    Console.WriteLine("  ---       ---  ---  --- ");
+                }
+            }
+        }
+
+        }
+    }
+
+           
